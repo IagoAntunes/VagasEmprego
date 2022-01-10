8d110da78009c209923c815858385eab97e5118b
iagoa <iagoantunes.f@gmail.com>
2022-01-10 16:00:33 -0300

    Cadastro Vagas

--- VagasEmprego/VagasEmprego/Paginas/ConsultaVagas.xaml.cs
+++ VagasEmprego/VagasEmprego/Paginas/ConsultaVagas.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using VagasEmprego.Modelos;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,21 @@ namespace VagasEmprego.Paginas
         public ConsultaVagas()
         {
             InitializeComponent();
+        }
+        private void GoCadastro(object sender,EventArgs args)
+        {
+            Navigation.PushAsync(new CadastroVaga());
+        }
+        private void GoMinhasVagas(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new MinhasVagasCadastradas());
+        }
+        private void MaisDetalheAction(object sender,EventArgs args)
+        {
+            Label lblDetalhe = (Label)sender;
+            TapGestureRecognizer tapGest = (TapGestureRecognizer)lblDetalhe.GestureRecognizers[0];
+            Vaga vaga = tapGest.CommandParameter as Vaga;
+            Navigation.PushAsync(new DetalheVaga(vaga));
         }
     }
 }
