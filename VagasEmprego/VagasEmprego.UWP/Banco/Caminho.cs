b5d07a0d7f29f6c3436b4f26142585d9ad5a9f08
iagoa <iagoantunes.f@gmail.com>
2022-01-10 14:50:28 -0300

    ICaminho  - UWP

---
+++ VagasEmprego/VagasEmprego.UWP/Banco/Caminho.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VagasEmprego.Banco;
+using Xamarin.Forms;
+using System.IO;
+using VagasEmprego.UWP.Banco;
+using Windows.Storage;
+
+[assembly: Dependency(typeof(Caminho))]
+namespace VagasEmprego.UWP.Banco
+{
+    public class Caminho : ICaminho
+    {
+        public string ObterCaminho(string NomeArquivoBanco)
+        {
+            string caminhoDaPasta = ApplicationData.Current.LocalFolder.Path;
+            string caminhoBanco = Path.Combine(caminhoDaPasta, NomeArquivoBanco);
+
+            return caminhoBanco;
+        }
+    }
+}
