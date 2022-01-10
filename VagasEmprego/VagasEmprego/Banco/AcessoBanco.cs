ffe25c635a595f8913fbac11c807a602dfd73a19
iagoa <iagoantunes.f@gmail.com>
2022-01-10 11:47:55 -0300

    DependencyService

---
+++ VagasEmprego/VagasEmprego/Banco/AcessoBanco.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using VagasEmprego.Modelos;
+using Xamarin.Forms;
+
+namespace VagasEmprego.Banco
+{
+    public class AcessoBanco
+    {
+
+        private SQLiteConnection _conexao;
+        public AcessoBanco()
+        {
+            var dep = DependencyService.Get<ICaminho>();
+            string caminho = dep.ObterCaminho("database.sqlite");
+            _conexao = new SQLiteConnection(caminho);
+        }
+
+        public void Cadastro(Vaga vaga)
+        {
+
+        }
+        public void Exclusao(int id)
+        {
+
+        }
+        public void Atualização(Vaga vaga)
+        {
+
+        }
+        public List<Vaga> Consultar()
+        {
+            return null;
+        }
+        public Vaga ObterVagaPorId(int id)
+        {
+            return null;
+        }
+
+
+
+
+    }
+}
---
+++ VagasEmprego/VagasEmprego/Banco/ICaminho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VagasEmprego.Banco
+{
+    public interface ICaminho
+    {
+        string ObterCaminho(string NomeArquivoBanco);
+
+
+
+
+    }
+}
