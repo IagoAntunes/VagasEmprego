93731656536e61cd12b1f807b008602b66494685
iagoa <iagoantunes.f@gmail.com>
2022-01-10 10:53:38 -0300

    Modelo - Vaga

---
+++ VagasEmprego/VagasEmprego/Modelos/Vaga.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VagasEmprego.Modelos
+{
+    public class Vaga
+    {
+        public string nomeVaga { get; set; }
+        public short Quantidade { get; set; }
+        public string Cidade { get; set; }
+        public double Salario { get; set; }
+        public string Descricao { get; set; }
+        public string TipoContratacao { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
