d96e20f64df4a2ec9ea138dc9ec6447599dc59fc
iagoa <iagoantunes.f@gmail.com>
2022-01-10 16:12:38 -0300

    Cadastro Vagas

--- VagasEmprego/VagasEmprego/Paginas/CadastroVaga.xaml.cs
+++ VagasEmprego/VagasEmprego/Paginas/CadastroVaga.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using VagasEmprego.Modelos;
+using VagasEmprego.Banco;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,28 @@ namespace VagasEmprego.Paginas
         public CadastroVaga()
         {
             InitializeComponent();
+        }
+        private void SalvarAction(object sender,EventArgs args)
+        {
+            //TODO - Obter dados da tela
+            Vaga vaga = new Vaga();
+            vaga.nomeVaga = NomeVaga.Text;
+            vaga.Quantidade = short.Parse(Quantidade.Text);
+            vaga.Salario = double.Parse(Salario.Text);
+            vaga.Empresa = Empresa.Text;
+            vaga.Descricao = Descricao.Text;
+            vaga.Cidade = Cidade.Text;
+            vaga.TipoContratacao = (TipoContratacao.IsToggled) ? "PJ" : "CLT";
+            vaga.Telefone = Telefone.Text;
+            vaga.Email = Email.Text;
+
+            //TODO - Salvar informações no banco
+            AcessoBanco database = new AcessoBanco();
+            database.Cadastro(vaga);
+
+            //TODO - Voltar para tela de pesquisa
+            Navigation.PopAsync();
+
         }
     }
 }
